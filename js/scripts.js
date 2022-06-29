5f820142538cd050fcfd4d86744b38e5b9d2b1f9
Daniel Macedo <danielmsv2011@hotmail.com>
2022-06-29 16:57:37 -0300

    Project Done

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,17 @@ const url = "https://jsonplaceholder.typicode.com/posts";
 const loadingElement = document.querySelector("#loading");
 const postsContainer = document.querySelector("#posts-container");
 
-const postPage = document.querySelector("#post")
-const postContainer = document.querySelector("#post-container")
-const commentsContainer = document.querySelector("#comments-container")
+const postPage = document.querySelector("#post");
+const postContainer = document.querySelector("#post-container");
+const commentsContainer = document.querySelector("#comments-container");
+
+const commentForm = document.querySelector("#comment-form");
+const emailInput = document.querySelector("#email");
+const bodyInput = document.querySelector("#body");
 
 // Get ID From URL
 const urlSearchParams = new URLSearchParams(window.location.search);
-const postId = urlSearchParams.get("id")
+const postId = urlSearchParams.get("id");
 
 // Getting all the posts
 async function getAllPosts() {
@@ -43,13 +47,11 @@ async function getAllPosts() {
 }
 
 // Getting a individual post
-async function getPost(id)  {
-
+async function getPost(id) {
   const [responsePost, responseComments] = await Promise.all([
     fetch(`${url}/${id}`),
-    fetch(`${url}/${id}/comments`)
-
-  ])
+    fetch(`${url}/${id}/comments`),
+  ]);
 
   const dataPost = await responsePost.json();
   const dataComments = await responseComments.json();
@@ -57,11 +59,70 @@ async function getPost(id)  {
   loadingElement.classList.add("hide");
   postPage.classList.remove("hide");
 
+  const title = document.createElement("h1");
+  const body = document.createElement("p");
 
+  title.innerText = dataPost.title;
+  body.innertext = dataPost.body;
+
+  postContainer.appendChild(title);
+  postContainer.appendChild(body);
+
+  console.log(dataComments);
+
+  dataComments.map((comment) => {
+    createComment(comment);
+  });
 }
 
-if(!postId) {
+function createComment(comment) {
+  const div = document.createElement("div");
+  const email = document.createElement("h3");
+  const commentBody = document.createElement("p");
+
+  email.innerText = comment.email;
+  commentBody.innerText = comment.body;
+
+  div.appendChild(email);
+  div.appendChild(commentBody);
+
+  commentsContainer.appendChild(div);
+}
+
+// Post a comment
+async function postComment(comment) {
+
+  const response = await fetch(`${url}/${postId}/comments`, {
+    method: "POST",
+    body: comment,
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+  });
+
+  const data = await response.json()
+
+  createComment(data)
+}
+
+
+if (!postId) {
   getAllPosts();
 } else {
-  getPost(postId)
+  getPost(postId);
+
+  // ADD Event to comment form
+  commentForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let comment = {
+      email: emailInput.value,
+      body: bodyInput.value,
+    };
+
+    comment = JSON.stringify(comment);
+
+    postComment(comment);
+  });
 }
